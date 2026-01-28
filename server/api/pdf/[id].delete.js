38184322c7e7ce8d4d9508ccda36b5cec67f0853
bhdn-19 <abi.hdn19@gmail.com>
2026-01-28 17:13:59 +0700

    remove

--- server/api/pdf/[id].delete.js
+++
@@ -1,9 +0,0 @@
-import { readData, writeData } from "~~/server/utils/helper"
-
-export default defineEventHandler(async (event) => {
-  const id = Number(event.context.params.id)
-  const data = await readData()
-
-  await writeData(data.filter(t => t.id !== id))
-  return { success: "ok" }
-})
--- server/api/pdf/[id].js
+++
@@ -1,8 +0,0 @@
-import { readData } from "~~/server/utils/helper"
-
-export default defineEventHandler(async (event) => {
-  const id = Number(event.context.params.id)
-  const data = await readData()
-  
-  return data.find(t => t.id === id)
-})
